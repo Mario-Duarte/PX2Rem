fd4b602b5d7a5e75f89e7b52b9474b4825ed0503
Mario Duarte <djtsunamimario@gmail.com>
2022-01-24 23:22:35 +0000

    Added link to websites

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { Container } from './styles';
 export function Footer() {
 return (
     <Container>
-    <p>Created by <a href='https://www.google.com/' target="_blank">Mario</a>&<a href='https://www.google.com/' target='_blank'>Alex</a>with &nbsp;<FaCode /> &nbsp; and &nbsp;<FaHeart color='red' /> </p>
+    <p>Created by <a href='http://mariodesigns.co.uk/' target="_blank">Mario</a>&<a href='https://aleex-caires-portfolio.vercel.app/' target='_blank'>Alex</a>with &nbsp;<FaCode /> &nbsp; and &nbsp;<FaHeart color='red' /> </p>
     </Container>
 )
 };
