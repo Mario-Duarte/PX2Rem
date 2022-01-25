5b057de7e4bf16ec3aad7fd53c4b8330742ba46c
Mario Duarte <djtsunamimario@gmail.com>
2022-01-25 00:30:14 +0000

    Updated footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,1 @@
-import { FaHeart, FaCode } from 'react-icons/fa';
-import { Container } from './styles';
-
-export function Footer() {
-return (
-    <Container>
-    <p>Created by <a href='http://mariodesigns.co.uk/' target="_blank">Mario</a>&<a href='https://aleex-caires-portfolio.vercel.app/' target='_blank'>Alex</a>with &nbsp;<FaCode /> &nbsp; and &nbsp;<FaHeart color='red' /> </p>
-    </Container>
-)
-};
+export { Footer } from './Footer';
