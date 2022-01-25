9a00c17f22a0eb4aaef6c294de585b9dfeaac601
Mario Duarte <djtsunamimario@gmail.com>
2022-01-25 00:31:19 +0000

    Started to add logic to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
-import { GlobalStyle } from './styles/GlobalStyle'
-
-import { Greetings } from './components/Greetings'
-
-import { Footer } from './components/Footer'
+import { useState } from 'react';
+import { GlobalStyle } from './styles/GlobalStyle';
+import { Title } from './components/Title';
+import { Footer } from './components/Footer';
+import { 
+  //useConvertUnits,
+  units,
+} from './Hooks/useConvertUnits';
+import { 
+  StyledApp,
+} from './App.styles';
 
 export function App() {
+
+  const [defaultSize, setDefaultSize] = useState<number>(16);
+  const [convertSize, setConvertSize] = useState<number>(16);
+  const [resultSize, setResultSize] = useState<number>(1);
+  const [convertUnit, setConvertUnit] = useState<units>('px');
+  const [resultUnit, setResultUnit] = useState<units>('rem');
+
+  const handleDefaultSize = (size:number) => {
+    setDefaultSize(size);
+  }
+
+  const handleConvertSize = (size:number) => {
+    setConvertSize(size);
+  }
+
+  const handleConvertUnit = (unit:units) => {
+    setConvertUnit(unit);
+  }
+
+  const handleResultUnit = (unit:units) => {
+    setResultUnit(unit);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Greetings />
-      <Footer />
+      <StyledApp>
+        <Title title='PX2REM' />
+        <Footer />
+      </StyledApp>
     </>
   )
 }
