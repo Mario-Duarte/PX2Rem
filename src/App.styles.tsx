830bbe1646362733ef7a81723c52e050c3cbf517
Mario Duarte <djtsunamimario@gmail.com>
2022-01-28 22:33:46 +0000

    Added Row and Col styled component to the app

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -5,3 +5,19 @@ export const StyledApp = styled.section`
     padding: 40px;
     height: 100vh;
 `;
+
+export const Row = styled.div`
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const Col = styled.div`
+    position: relative;
+    width: calc(50% - 5px);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+`;
