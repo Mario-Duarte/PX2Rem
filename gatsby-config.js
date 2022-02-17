64840787ceba0d41f05d1fe8b963d4cab820656e
Mario Duarte <djtsunamimario@gmail.com>
2022-02-17 20:58:32 +0000

    Added styled components to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,14 @@ module.exports = {
       title: `PX2REM`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet", {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    },
+    'gatsby-plugin-styled-components'
+  ]
 };
