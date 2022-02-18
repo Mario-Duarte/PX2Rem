2b76950259d4ac8e62d37f0ee0e6620eb3a2fe11
Mario Duarte <djtsunamimario@gmail.com>
2022-02-18 20:39:59 +0000

    Added input size component

---
+++ src/components/InputSize/InputSize.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { 
+    InputContainer,
+    StyledInput,
+    Label
+} from "../../theme/shared"
+
+export function InputSize({value, onChange}) {
+
+    const handleChange = (e) => {
+        if (e.target.value) {
+            const size = +e.target.value;
+            onChange(size === 0 ? 1 : size);
+        }
+    }
+
+    return(
+        <InputContainer>
+            <StyledInput aria-label='convertSize' type="number" step={1} value={value} min={1} onChange={handleChange} />
+            <Label>Convert</Label>
+        </InputContainer>
+    )
+}
