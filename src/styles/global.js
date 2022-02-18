5c111767da97c9ccf0b916463fb7eb5654c69fee
Mario Duarte <djtsunamimario@gmail.com>
2022-02-18 00:57:38 +0000

    Global and theme style changes

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,10 +5,11 @@ export const GlobalStyle = createGlobalStyle`
     body {
         padding: 0;
         margin: 0;
-        min-height: 100%;
+        min-height: 100vh;
         background-color: #264449;
         background: linear-gradient(180deg, rgba(70,57,59,1) 0%, rgba(38,68,73,1) 100%);
         font-family: 'Rubik', sans-serif;
+        font-size: 16px;
     }
 
     a {
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,20 +1,20 @@
 import { breakpoints } from "./breakpoints"
 
 export const theme = {
-    color: {
+    colors: {
         lightCopy: '#EEEEEE',
         darkCopy: '#555555',
         primaryColor: '#46393B',
         secondaryColor: '#264449',
         tertiaryColor: '#EEC0C6',
+        otherColor: '#88E5F6',
         heartColor: '#C70E26',
+        lightGradient: 'linear-gradient(45deg, #EEC0C6 0%, #88E5F6 100%)',
     },
-    breakpoints: {
-        breakpoints,
-    },
+    breakpoints,
     media: {
-        sm: `(min-width: ${breakpoints.sm})`,
-        lg: `(min-width: ${breakpoints.lg})`,
-        xl: `(min-width: ${breakpoints.xl})`,
+        sm: `@media (min-width: ${breakpoints.sm}px)`,
+        lg: `@media (min-width: ${breakpoints.lg}px)`,
+        xl: `@media (min-width: ${breakpoints.xl}px)`,
     }
 }
