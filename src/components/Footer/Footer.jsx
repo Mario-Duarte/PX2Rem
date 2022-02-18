3913f0be117477ba6d6e81ccc38c18af3c7d717b
Mario Duarte <djtsunamimario@gmail.com>
2022-02-18 01:01:58 +0000

    Remove unused templates

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <div>
-      <h1>This is the Footer</h1>
-  </div>;
-};
-
-export default Footer;
--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-  return <div>
-      <h1>This is the header</h1>
-  </div>;
-};
-
-export default Header;
