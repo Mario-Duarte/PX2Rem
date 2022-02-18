def0387af53cc0e7a8d2b95b3c3fa0f374f6773c
Mario Duarte <djtsunamimario@gmail.com>
2022-02-18 00:57:08 +0000

    Added logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledLogo } from "./Logo.styles";
+
+export function Logo() {
+    return(
+        <>
+            <StyledLogo>PX2REM</StyledLogo>
+        </>
+    )
+}
---
+++ src/components/Logo/Logo.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const StyledLogo = styled.h1`
+    color: ${p => p.theme.colors.lightCopy};
+    font-size: 1.5rem;
+    letter-spacing: -0.063rem;
+    margin: 0;
+`;
