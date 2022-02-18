702ff7124f22bb26a70e80d05514544d341e76ea
Mario Duarte <djtsunamimario@gmail.com>
2022-02-18 20:46:59 +0000

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,53 +1,38 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
+import { Header } from "../components/Header/Header"
+import { Logo } from "../components/Logo/Logo"
+import { Buttons } from "../components/Buttons/Buttons"
+import { Main } from "../components/Container/Main"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <Header>
+        <Logo />
+        <Buttons href={"https://google.com"} className="small" id="hello">
+          <>Download PX2REM</>
+        </Buttons>
+      </Header>
+
+      <Main>
+        <title>Not found</title>
+        <h1>We couldn't convert the size you are looking for...</h1>
+        <p>
+          Sorry{" "}
+          <span role="img" aria-label="Pensive emoji">
+            😔
+          </span>{" "}
+          we couldn't find what you were looking for but we can can help you find your way again.
+          <br />
+          <br />
+          <Link to="/">Go to home</Link>.
+        </p>
+      </Main>
+
+    </Layout>
   )
 }
 
